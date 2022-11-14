f06d6ff1eee93103471756c503139455b7f5eee0
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-14 03:46:57 +0000

    [Add]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about' => 'homes#about', as: 'about'
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+  end
   resources :users, only: [:show, :edit, :update]
 end
